b5b0c405892e2b168ee644f349e0954c9a6c8786
Artem <artem-gavrilin-00@mail.ru>
2020-08-27 16:05:40 +0300

    Автоматизировано заполнение Фактического Блока и Разницы

--- TestSoliution/TestAddin/InputForm.cs
+++ TestSoliution/TestAddin/InputForm.cs
@@ -16,7 +16,7 @@ namespace TestAddin
     public partial class InputForm : Form
     {
         public static int currentCollum = 0;
-        public static int entryDateCollum = 2, departureDateCollum = 4, roomCategoryCollum = 5;
+        public static int entryDateCollum = 6, departureDateCollum = 8, roomCategoryCollum = 5;
         public InputForm()
         {
             InitializeComponent();
@@ -33,26 +33,39 @@ namespace TestAddin
             Hotel.categoryCount = Convert.ToInt32(textBox2.Text);
             Hotel.entry = dateTimePicker1.Value;
             Hotel.departure = dateTimePicker2.Value;
+            Hotel.currentStr = 0;
 
             Worksheet currentWorksheet = Globals.ThisAddIn.Application.ActiveSheet;
+            currentWorksheet.Cells.Clear();
+            currentWorksheet.Name = Hotel.name;
             string entryDate = dateTimePicker1.Value.ToShortDateString();
             string departureDate = dateTimePicker2.Value.ToShortDateString();
 
             //currentWorksheet.Cells[1, 1] = temp2.AddDays(1);
             //currentWorksheet.Cells[1, 2] = "Категория";
             //PrintHead(ref currentWorksheet);
-            PrintLittleHead(ref currentWorksheet);
 
-            for (int i = Hotel.currentStr; i < Hotel.categoryCount + 1; ++i)
+            PrintLittleHead(ref currentWorksheet, "Блок в отеле");
+            /*for (int i = Hotel.currentStr; i < Hotel.categoryCount + Hotel.currentStr; ++i)
             {
-                for (int j = 0; j < 9; ++j)
+                for (int j = 3; j < 12; ++j)
                 {
-                    if (j == entryDateCollum) { currentWorksheet.Cells[i + 1, j + 1] = entryDate; }
-                    else if (j == departureDateCollum) { currentWorksheet.Cells[i + 1, j + 1] = departureDate; }
+                    if (j == entryDateCollum && i != 0) { currentWorksheet.Cells[i + 1, j + 1] = entryDate; }
+                    else if (j == departureDateCollum && i != 0) { currentWorksheet.Cells[i + 1, j + 1] = departureDate; }
 
                 }
-                if (i == Hotel.categoryCount-1) { Hotel.currentStr = i+3; }
-            }
+                if (i == Hotel.categoryCount-1) { Hotel.currentStr += Hotel.categoryCount; }
+            }*/
+            FillingLittleTable(ref currentWorksheet);
+
+            
+            PrintLittleHead(ref currentWorksheet, "Фактический блок");
+            FillingLittleTable(ref currentWorksheet);
+
+            //Hotel.currentStr += Hotel.categoryCount+1;
+            PrintLittleHead(ref currentWorksheet, "Разница");
+            FillingLittleTable(ref currentWorksheet);
+
             PrintBigHead(ref currentWorksheet);
             currentWorksheet.Columns.AutoFit();
             this.Close();
@@ -93,31 +106,45 @@ namespace TestAddin
 
         }
 
-        public void PrintLittleHead(ref Worksheet currentWorksheet)
+        public void PrintLittleHead(ref Worksheet currentWorksheet, string title)
         {
-            currentWorksheet.Cells[1, 1] = "Название категории";
-            currentWorksheet.Cells[1, 3] = "Заезд";
-            currentWorksheet.Cells[1, 5] = "Выезд";
-            int tempCollum = 7;
-            PrintDates(currentWorksheet, 1, ref tempCollum);
+            currentWorksheet.Cells[Hotel.currentStr+1, 4].Value2 = title;
+            int temp = Hotel.currentStr+1;
+            int strPointer = 2;
+            for(int i = 0; i < Hotel.categoryCount; ++i)
+            {
+                currentWorksheet.Cells[temp+1, 5].Value2 = i + 1;
+                if(title != "Блок в отеле")
+                {
+                    string formula = "=F" +strPointer;
+                    currentWorksheet.Cells[temp + 1, 6].Formula = formula;
+                    ++strPointer;
+                }
+                ++temp;
+            }
+            currentWorksheet.Cells[Hotel.currentStr+1, 6] = "Название категории";
+            currentWorksheet.Cells[Hotel.currentStr+1, 7] = "Въезд";
+            currentWorksheet.Cells[Hotel.currentStr+1, 9] = "Выезд";
+            int tempCollum = 11;
+            PrintDates(currentWorksheet, Hotel.currentStr+1, ref tempCollum);
             tempCollum++;
-            currentWorksheet.Cells[1, tempCollum] = "Одноместный";
+            currentWorksheet.Cells[Hotel.currentStr+1, tempCollum] = "Одноместный";
             tempCollum++;
-            currentWorksheet.Cells[1, tempCollum] = "Двухместный";
+            currentWorksheet.Cells[Hotel.currentStr+1, tempCollum] = "Двухместный";
         }
 
         public void PrintBigHead(ref Worksheet currentWorksheet)
         {
-            currentWorksheet.Cells[Hotel.currentStr, 1] = "Объект Размещения";
-            currentWorksheet.Cells[Hotel.currentStr, 2] = "Группа";
-            currentWorksheet.Cells[Hotel.currentStr, 3] = "Номер комнаты";
-            currentWorksheet.Cells[Hotel.currentStr, 4] = "ФИО";
-            currentWorksheet.Cells[Hotel.currentStr, 5] = "Кол-во чел. в номере";
-            currentWorksheet.Cells[Hotel.currentStr, 6] = "Номер";
-            currentWorksheet.Cells[Hotel.currentStr, 7] = "Заезд";
-            currentWorksheet.Cells[Hotel.currentStr, 9] = "Выезд";
+            currentWorksheet.Cells[Hotel.currentStr+1, 1] = "Объект Размещения";
+            currentWorksheet.Cells[Hotel.currentStr+1, 2] = "Группа";
+            currentWorksheet.Cells[Hotel.currentStr+1, 3] = "Номер комнаты";
+            currentWorksheet.Cells[Hotel.currentStr+1, 4] = "ФИО";
+            currentWorksheet.Cells[Hotel.currentStr+1, 5] = "Кол-во чел. в номере";
+            currentWorksheet.Cells[Hotel.currentStr+1, 6] = "Номер";
+            currentWorksheet.Cells[Hotel.currentStr+1, 7] = "Въезд";
+            currentWorksheet.Cells[Hotel.currentStr+1, 9] = "Выезд";
             int tempCollum = 11;
-            PrintDates(currentWorksheet, Hotel.currentStr, ref tempCollum);
+            PrintDates(currentWorksheet, Hotel.currentStr+1, ref tempCollum);
         }
 
         public void PrintDates(Worksheet currentWorksheet, int str, ref int collum)
@@ -131,6 +158,20 @@ namespace TestAddin
                 currentWorksheet.Cells[str, collum] = temp.ToShortDateString();
                 ++collum;
             };
+        }
+
+        public void FillingLittleTable(ref Worksheet worksheet)
+        {
+            for (int i = Hotel.currentStr+1; i <= Hotel.categoryCount + Hotel.currentStr; ++i)
+            {
+                for (int j = 3; j < 12; ++j)
+                {
+                    if (j == entryDateCollum && i != 0) { worksheet.Cells[i + 1, j + 1] = Hotel.entry.ToShortDateString(); }
+                    else if (j == departureDateCollum && i != 0) { worksheet.Cells[i + 1, j + 1] = Hotel.departure.ToShortDateString(); }
+
+                }
+            }
+            Hotel.currentStr += Hotel.categoryCount + 2;
         }
     }
 }
--- TestSoliution/TestAddin/TestRibbon.cs
+++ TestSoliution/TestAddin/TestRibbon.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using Microsoft.Office.Tools.Ribbon;
 using Microsoft.Office.Interop.Excel;
-using _Excel = Microsoft.Office.Interop.Excel;
 using System.Windows.Forms;
 
 namespace TestAddin
@@ -12,7 +11,28 @@ namespace TestAddin
     
     public partial class TestRibbon 
     {
-        
+        Dictionary<int, string> collumNames = new Dictionary<int, string>
+        {
+            { 11, "K" },
+            { 12, "L"},
+            { 13, "M"},
+            { 14, "N"},
+            { 15, "O"},
+            { 16, "P"},
+            { 17, "Q"},
+            { 18, "R"},
+            { 19, "S"},
+            { 20, "T"},
+            { 21, "U"},
+            { 22, "V"},
+            { 23, "W"},
+            { 24, "X"},
+            { 25, "Y"},
+            { 26, "Z"},
+            { 27, "AA"},
+            { 28, "AB"},
+            { 29, "AC"},
+        };
         private void TestRibbon_Load(object sender, RibbonUIEventArgs e)
         {
 
@@ -27,8 +47,15 @@ namespace TestAddin
 
         private void button2_Click(object sender, RibbonControlEventArgs e)
         {
+            //Workbook workbook = (Microsoft.Office.Interop.Excel.Application)Marshal.GetActiveObject("Excel.Application");
+            //Workbook workbook = Globals.ThisAddIn.Application.Workbooks[0] ;
             Worksheet currentWorksheet = Globals.ThisAddIn.Application.ActiveSheet;
             //currentWorksheet.Cells[1, 13] = Hotel.name;
+            /*if(workbook.Worksheets[2] == null)
+            {
+                workbook.Worksheets.Add(After: currentWorksheet);
+
+            }*/
             FillingBigTable(ref currentWorksheet);
             currentWorksheet.Cells.AutoFit();
         }
@@ -43,6 +70,7 @@ namespace TestAddin
             {
                 worksheet.Cells[i, 1].Value2 = Hotel.name;
             }*/
+            Hotel.currentStr += 1;
             for(int i = 0; i < Hotel.categoryCount; ++i)
             {
                 int temp = 0;
@@ -55,8 +83,9 @@ namespace TestAddin
                     Hotel.currentStr += 1;
                     ++temp;
                 }
-                temp = 0;
             }
+            FactBlock(ref worksheet, ref roomsCount);
+            DifferenceBlock(ref worksheet);
             /*for(int i = Hotel.currentStr+1; i < roomsCount[Hotel.categoryCount]; ++i)
             {
                 worksheet.Cells[i, 1] = Hotel.name;
@@ -75,12 +104,64 @@ namespace TestAddin
             int temp = 0;
             for(int i = 1; i <= Hotel.categoryCount; ++i)
             {
-                categories.Add(worksheet.Cells[i+1, 1].Value2);
-                roomsCount.Add(Convert.ToInt32(worksheet.Cells[i+1, 7].Value2));
+                categories.Add(worksheet.Cells[i+1, 6].Value2);
+                roomsCount.Add(Convert.ToInt32(worksheet.Cells[i+1, 11].Value2));
                 temp += roomsCount[i - 1];
             }
             roomsCount.Add(temp);
             //worksheet.Cells[12, 12].Value2 = categories[0];
+        }
+
+        public void FactBlock(ref Worksheet worksheet, ref List<int> roomsCount)
+        {
+            int str = Hotel.categoryCount + 4;
+            int tempStr = Hotel.categoryCount * 3 + 8;
+            int category = 0;
+            string collumName = "";
+            DateTime departureDate = Hotel.departure;
+            for (int i = str; i < str + Hotel.categoryCount; ++i)
+            {
+                int collum = 11;
+                for (DateTime temp = Hotel.entry; temp.ToShortDateString() != departureDate.AddDays(1).ToShortDateString(); temp = temp.AddDays(1))
+                {
+                    if (collumNames.TryGetValue(collum, out collumName))
+                    {
+                        string formula = "=SUM(" + collumName + tempStr + ":" + collumName;
+                        int tempInt = tempStr + roomsCount[category] - 1;
+                        formula += tempInt + ")";
+                        worksheet.Cells[i, collum].Formula = formula;
+                    }
+                    //worksheet.Cells[i, collum] = temp.ToShortDateString();
+                    ++collum;
+                }
+                tempStr += roomsCount[category];
+                ++category;
+            }
+        }
+
+        public void DifferenceBlock(ref Worksheet worksheet)
+        {
+            int str = Hotel.categoryCount * 2 + 6;
+            int tempStr = 2;//+Hotel.categoryCount + 2
+            //int category = 0;
+            string collumName = "";
+            DateTime departureDate = Hotel.departure;
+            for (int i = str; i < str + Hotel.categoryCount; ++i)
+            {
+                int collum = 11;
+                for (DateTime temp = Hotel.entry; temp.ToShortDateString() != departureDate.AddDays(1).ToShortDateString(); temp = temp.AddDays(1))
+                {
+                    if (collumNames.TryGetValue(collum, out collumName))
+                    {
+                        string formula = "=" + collumName + tempStr + " - " + collumName + (tempStr + Hotel.categoryCount + 2);
+                        //formula +=  + ")";
+                        worksheet.Cells[i, collum].Formula = formula;
+                    }
+                    //worksheet.Cells[i, collum] = temp.ToShortDateString();
+                    ++collum;
+                };
+                ++tempStr;
+            }
         }
     }
 }
